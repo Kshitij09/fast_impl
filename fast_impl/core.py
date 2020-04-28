831cfe156ec37111c8c71f72028ad89868084cbf
Kshitij Patil <kshitijpatil98@gmail.com>
2020-04-28 21:33:27 +0530

    Update `arch_summary`
    * Summary now include module names, if available.
    * Added more examples in `arch_explore` tutorial

--- fast_impl/core.py
+++ fast_impl/core.py
@@ -4,6 +4,7 @@ __all__ = ['get_module', 'arch_summary']
 
 # Cell
 from fastai2.vision.all import *
+import pdb
 
 # Cell
 def _is_sequential(o): return o.__class__.__name__=='Sequential'
@@ -11,28 +12,26 @@ def _is_fn(o): return isinstance(o,(types.FunctionType, functools.partial))
 
 # Cell
 def get_module(o,i):
-  "Recursively get the module from list of indices"
-  if is_listy(i):
-    m = get_module(o,i[0])
-    if len(i)==1: return m
-    return get_module(m,i[1:])
-  return o[i] if is_listy(o) else list(o.children())[i]
+    "Recursively get the module from list of indices"
+    o = o(False) if _is_fn(o) else o
+    if is_listy(i):
+        m = get_module(o,i[0])
+        if len(i)==1: return m
+        return get_module(m,i[1:])
+    assert i < len(list(o.children())), f"IndexError: {i} is out of bounds"
+    return o[i] if is_listy(o) else list(o.children())[i]
 
 # Cell
-def arch_summary(arch,idx=None,verbose:bool=False):
-  r"""Short architecture summary, used for holistic understanding of model
-  Args:
-      arch: a function or model object
-      idx (int,list): an integer or list of indices to reach desired module in the architecture
-      verbose (bool): If True, will list the names of sub-modules
-  """
+def arch_summary(arch,idx=None,verbose=False):
+  "Short architecture summary, used for holistic understanding and deciding parameter groups"
   model = arch(False) if _is_fn(arch) else arch
   if idx is not None:
     model = get_module(model,idx)
-  for i, l in enumerate(model.children()):
+  for i,(n,l) in enumerate(model.named_children()):
       n_layers = len(l if _is_sequential(l) else flatten_model(l))
-      print(f'({i:<2}) {l.__class__.__name__:<17}: {n_layers:<4}layers')
+      pre = f'[{n:<2}]' if n.isdigit() else f'[{i:<2}] {"("+n+")":<10}'
+      print(f'{pre} {l.__class__.__name__:<17}: {n_layers:<3} layers')
       if verbose and l.has_children:
         layers = [x.__class__.__name__ for x in l.children()]
         for il in layers:
-          print(" "*5,il)
+          print(" "*(5 + (15 if not n.isdigit() else 0)),il)
