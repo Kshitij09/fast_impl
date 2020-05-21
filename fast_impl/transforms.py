98997082e80e75fe41c7a88fa6284a5493ce6f08
Kshitij Patil <kshitijpatil98@gmail.com>
2020-05-21 18:46:53 +0530

    minor changes in GCN
    * min_max_scale to norm 
    * Normalize with CIFAR stats
    * Fix transform order
    * Remove `GetAttr` as parent class of ZCAWhitenWrapper
    
    Signed-off-:by Kshitij Patil <kshitijpatil98@gmail.com>

--- fast_impl/transforms.py
+++ fast_impl/transforms.py
@@ -22,7 +22,7 @@ class GlobalContrastNorm(Transform):
   reference: Deep Learning book (section 12.2.1.1 pg.455)
   decodes used for `show_batch`, not to reverse the transform.
   """
-  order=11 # Need to be applied after IntToFloatTensor
+  order=104 # Need to be applied after Normalize
   def __init__(self,s=1,eps=1e-8,lmbda=0.):
     store_attr(self,'s,eps,lmbda')
 
@@ -35,12 +35,12 @@ class GlobalContrastNorm(Transform):
     contrast = (x.var((1,2,3),keepdim=True)+self.lmbda).sqrt().clamp_(self.eps)
     return self.s * (x - x.mean((1,2,3),keepdim=True)) / contrast
 
-  def decodes(self,x:TensorImage): return min_max_scale(x)
+  def decodes(self,x:TensorImage): return norm(x)
 
 # Cell
-class ZCAWhitenWrapper(Transform,GetAttr):
+class ZCAWhitenWrapper(Transform):
   "Wrapping kornia implementation"
-  _default='_zca'
+  order=108 # Need to be applied after Normalize and GCN
   @delegates(kornia.color.ZCAWhitening)
   def __init__(self,**kwargs):
     self._zca = ZCAWhitening(**kwargs)
@@ -54,4 +54,4 @@ class ZCAWhitenWrapper(Transform,GetAttr):
   def decodes(self,x:TensorImage):
     if self._zca.compute_inv:
       x = self._zca.inverse_transform(x)
-    return min_max_scale(x)
+    return norm(x)
